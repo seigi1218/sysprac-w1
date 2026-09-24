86857fa60067d6e3ba62104e7f0d83a044068222
seigi <a0985497556@gmail.com>
2026-09-24 10:08:52 +0800

    first commit

---
+++ py.py
@@ -0,0 +1,17 @@
+sensor_id="???"
+temperature=12.33
+count=int(temperature)
+
+print(sensor_id)
+print(temperature)
+print(count)
+
+print(type(sensor_id))
+print(type(temperature))
+print(type(count))
+
+reading = "30.5"
+nbb=float(reading)
+
+print(nbb+0.5)
+#11
